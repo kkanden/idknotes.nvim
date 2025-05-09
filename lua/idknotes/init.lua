3861f0063e187896ada4f18475df257b667b29ee
oliwia <24637207+kkanden@users.noreply.github.com>
2025-05-09 13:40:53 +0200

    feat: per-project notes

--- lua/idknotes/init.lua
+++ lua/idknotes/init.lua
@@ -5,6 +5,8 @@ local folder_path = vim.fs.joinpath(vim.fn.stdpath("data"), "idknotes")
 local global_note_path = vim.fs.joinpath(folder_path, "global.md")
 local data_path = vim.fs.joinpath(folder_path, "idknotes.json")
 
+M.data = utils.read_data()
+
 local state = {
     floating = {
         buf = -1,
@@ -35,7 +37,7 @@ local function open_floating_window(opts)
         row = row,
         style = "minimal",
         border = "rounded",
-        title = " idk notes ",
+        title = opts.title,
         title_pos = "center",
     }
 
@@ -44,59 +46,92 @@ local function open_floating_window(opts)
     return { buf = buf, win = win }
 end
 
-local function create_note(path) vim.fn.writefile({ "" }, path) end
+local function create_note(global, name)
+    local path = global and global_note_path
+        or vim.fs.joinpath(folder_path, name .. ".md")
 
-local function read_data()
-    local lines = vim.fn.readfile(data_path)
-    return vim.json.decode(lines)
-end
-
-local function write_data(data)
-    local json = vim.json.encode(data)
-    vim.fn.writefile({ json }, data_path)
+    vim.fn.writefile({ "" }, path)
 end
 
 local function get_project_name()
+    local data = utils.read_data()
+    local project_path = utils.resolve_project_path()
+    return data[project_path]
+end
+
+local function add_project_note(name, project_path)
+    create_note(false, name)
+
+    M.data = vim.tbl_extend("keep", M.data, { [project_path] = name })
+    utils.write_data(M.data)
+end
+
+local function create_project_name(project_path)
     vim.ui.input({ prompt = "Enter project name: " }, function(input)
         if not input then return end
-        local data = read_data()
 
-        -- check if project name is already used
-        for key, _ in pairs(data) do
-            if input == key then
-                vim.notify("Project name already used.", vim.log.levels.ERROR)
-                return
-            end
-        end
+        input = input:match("^%s*(.*)%s*$") -- strip whitespace
 
-        local working_dir = vim.cmd("pwd")
-        local root_dir = vim.fs.root(pwd, ".git") -- per-project notes will work on git repositories
-        if not root_dir then
-            vim.notify(
-                "Not in a git repository. Can't create a project note.",
-                vim.log.levels.ERROR
-            )
+        if input == "global" then
+            vim.notify([[Cannot use name "global".]], vim.log.levels.ERROR)
             return
         end
 
-        data = vim.tbl_extend("keep", data, { [input] = root_dir })
-        write_data(data)
+        -- check if project name is already used
+        if vim.tbl_contains(M.data, input) then
+            vim.notify("Project name already used.", vim.log.levels.ERROR)
+            return
+        end
+
+        add_project_note(input, project_path)
     end)
 end
 
 function M.toggle_notes(global)
-    global = global or true
-    local path = global and global_note_path
+    -- close window
     if vim.api.nvim_win_is_valid(state.floating.win) then
-        -- close window
         vim.api.nvim_win_hide(state.floating.win)
-    else
-        -- open window
-        if not utils.readable(path) then create_note(path) end
-        state.floating =
-            open_floating_window({ buf = state.floating.buf, global = global })
-        vim.cmd("edit " .. path)
+        return
     end
+
+    global = global == nil and true
+
+    local project_name
+    local project_path
+    if not global then
+        project_name = get_project_name()
+        project_path = utils.resolve_project_path()
+    end
+
+    if not project_path and not global then
+        vim.notify(
+            "Not in a git repository - can't open or create a project note",
+            vim.log.levels.WARN
+        )
+        return
+    end
+
+    if not project_name and not global then
+        vim.ui.input(
+            { prompt = "No note found for current project. Create? [y/n]" },
+            function(input)
+                if not input or not input:match("y") then return end
+                create_project_name(project_path)
+            end
+        )
+        return
+    end
+
+    local path = global and global_note_path
+        or vim.fs.joinpath(folder_path, get_project_name() .. ".md")
+
+    -- open window
+    state.floating = open_floating_window({
+        buf = state.floating.buf,
+        title = global and " global notes "
+            or string.format(" %s project notes ", project_name),
+    })
+    vim.cmd("edit " .. path)
 end
 
 function M.setup(opts)
@@ -105,7 +140,10 @@ function M.setup(opts)
     -- setup directory and data json file if they don't exist
     if vim.fn.isdirectory(folder_path) == 0 then vim.fn.mkdir(folder_path) end
     if not utils.readable(data_path) then
-        vim.fn.writefile(vim.json.encode({ global = "GLOBAL" }), data_path)
+        vim.fn.writefile(
+            { vim.json.encode({ [global_note_path] = "global.md" }) },
+            data_path
+        )
     end
 
     vim.api.nvim_create_user_command("IDKnotes", M.toggle_notes, {})
--- lua/idknotes/utils.lua
+++ lua/idknotes/utils.lua
@@ -1,5 +1,24 @@
 local M = {}
 
+local folder_path = vim.fs.joinpath(vim.fn.stdpath("data"), "idknotes")
+local data_path = vim.fs.joinpath(folder_path, "idknotes.json")
+
 function M.readable(file) return vim.fn.filereadable(file) == 1 end
+
+function M.read_data()
+    local lines = vim.fn.readfile(data_path)
+    lines = table.concat(lines) -- vim.json.decode needs a pure string
+    return vim.json.decode(lines)
+end
+
+function M.write_data(data)
+    local json = vim.json.encode(data)
+    vim.fn.writefile({ json }, data_path)
+end
+
+function M.resolve_project_path()
+    local working_dir = vim.cmd("pwd")
+    return vim.fs.root(working_dir, ".git") -- per-project notes will work on git repositories
+end
 
 return M
